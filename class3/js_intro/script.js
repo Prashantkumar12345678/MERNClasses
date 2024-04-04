97c2487d43ff8596e88b077fcb6d4504f06ba8ec
Prashant Kumar <142380904+Prashantkumar12345678@users.noreply.github.com>
2024-04-04 16:31:12 +0530

    class3

---
+++ class3/js_intro/script.js
@@ -0,0 +1,207 @@
+// console.log("hello");
+// function env(){
+// var message="hello wrold!"
+// var age=10;
+// age="Done" 
+// console.log(message);
+//  var message="else";
+//  console.log(message);
+//  console.log(age)
+
+// }
+//  console.log(message);
+//  function env(){
+//     var message="hello "
+//     console.log(message);
+//     var message="else "
+//     console.log(message);
+    
+    
+    
+
+//  }
+//  env();
+
+// let message="hello";
+// console.log(message);
+
+// const  message;
+// message="hello";
+// console.log(message);
+//   let age="20";
+//   age="abcd";
+//  console.log( typeof(age));
+//  console.log(age);
+
+// let userAge='abcd';
+// userAge='abbd';
+// console.log(userAge);
+
+
+
+// let arr=['a' ,'b', 'c', 'd'];
+// arr[0]='abcd';
+// console.log(arr);
+//  let src='12';
+// let age=20;
+
+// // let ans=age-src;
+// // console.log(ans);
+// let anss=age+src;
+// console.log(anss)
+// let ans=age-(-src);
+// console.log(ans);
+
+//typechecking
+
+// let a="12";
+// let b=12;
+
+// if(a=b){
+//     console.log("yes");
+
+// }else{
+//     console.log('no');
+// }
+//  if(a==b{
+//     console.log("yes again");
+//  }
+//  else{
+//     console.log("no again");
+//  }
+
+// let a=['a','b','c','d']
+// let cpy=[...a];
+// console.log(a, cpy);
+
+// if(a==cpy){
+//     console.log('1.yes');
+// }
+// else{
+//     console.log('1.no');
+// }
+// if(a===cpy){
+//     console.log('2.yes');
+// }
+// else{
+//     console.log('2.no');
+// }
+
+
+// const str='prashant chaudhary';
+
+// const name='My name is :';
+
+// const ans = name+str;
+// const ans=`${name} ${str}`; 
+// const anss= name+ ' ' +str;    // efficient way to write the code
+// console.log(ans);
+// console.log(anss);
+//  output = My name is : prashant chaudhary
+
+
+// -----write functions---
+// function print(str ='nothing is here'){
+//     console.log(str)
+
+// }
+// const s="hello";
+// print('hello');
+// print()
+
+// let a=1;
+// if(a){
+//     console.log('yes')
+
+// } else
+// {
+//     console.log('no')
+// }
+// function sum(a,b){
+//     if(a&&b){
+
+//         console.log(a+b);
+//     } else if(a){
+//         console.log(a);
+//     }
+//     else{
+//         console.log(0);
+//     }
+
+// }
+// sum(0);
+// sum(2);
+// sum(2,5);
+
+
+// function  declaration
+// function print(){
+//     console.log('__');
+// }
+
+
+// function print(){
+//     console.log('__');
+// }
+// //function assignment(name)
+// const a=function print(){
+//     console.log('__');
+// }
+// //function assignment(anonymous)
+// const b=function print(){
+//     console.log('__');
+// }
+// //arrow function assignment
+// const c=() =>{
+//     console.log('__');
+// }
+
+// print(a);
+// print(b);
+// print(c);
+
+
+// let a=2;
+//  switch(a){
+//     case 1:
+//     {
+//       console.log('one');
+//       break;
+//     }
+//      case 2:
+//      {
+//         console.log('two');
+//         break;
+        
+
+//      }
+//      case 3:
+//      {
+//         console.log('three');
+//      }
+//      default:{
+//         console.log(' not 1 2 3');
+//      }
+//  }
+
+// const obj = new Object();
+// const obj = {};
+//  obj[1]='one';
+//  obj[2]='two';
+//  obj[3]='three';
+// console.log(obj);
+
+// const obj = {
+//     1:'one', 'firstname':'Panwar',  'lastname':'Kumar', 12:'twelve',
+// };
+//  const fullname = obj.firstname +' '+obj.lastname;
+// console.log(fullname);
+
+
+// const obj = {
+//         1:'one', 'name':'Panwar',  'lname':'Kumar', 12:'twelve', 'address':'delhi'
+//      };
+//      const x = prompt(
+//         'plese tell me the property to know'
+//      );
+//      console.log(obj[x]);
